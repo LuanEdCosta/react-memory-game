a01c020410fbca0751c4fc2f2aa6f1c5e817e7e2
Luan Eduardo da Costa <luan1346@gmail.com>
2020-04-22 22:03:27 -0300

    Using default theme type in App

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -5,11 +5,12 @@ import DificultyChooser from '../DifficultyChooser'
 import GlobalStyles from '../../styles/Global'
 import { useTypedSelector } from '../../hooks'
 import THEME_COLORS from '../../styles/Theme'
+import { ThemeTypes } from '../../types/Theme'
 import Game from '../Game'
 
 const App: React.FC = () => {
   const themeType = useTypedSelector(({ Theme }) => Theme.type)
-  const themeColors = THEME_COLORS[themeType]
+  const themeColors = THEME_COLORS[themeType || ThemeTypes.dark]
 
   return (
     <ThemeProvider theme={themeColors}>
